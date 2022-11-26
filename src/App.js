40bc68ee66686026429e81cfa3963db2972d96fb
lvmh <lvminghui1001@126.com>
2022-11-26 16:28:48 +0800

    112601

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Go1
+          Learn React Go Go
         </a>
       </header>
     </div>
